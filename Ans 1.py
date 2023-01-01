3908d03b6d4cfabd214ab5831d760696249d9822
CompetitiveCell <87491219+CompetitiveCell@users.noreply.github.com>
2023-01-01 15:04:13 +0530

    Update Ans 1.py

--- Ans 1.py
+++ Ans 1.py
@@ -1,21 +1,19 @@
 file = open("HashTextFile.txt", "w")
-file_data = []
-choice = int(input("Enter how many lines to enter: "))
 
-def write_file(choice):
+def write_file():
+    file_data = []
+    choice = int(input("Enter how many lines to enter: "))
     for i in range(choice):
         line = input("Write any line : ")
         file_data.append(line + '\n')
-        file.writelines(file_data)
-    file.close()
+    file.writelines(file_data)
 
-write_file(choice)
+write_file()
 
 file = open("HashTextFile.txt", "r")
 data = " "
-while data:
-    data = file.readline()
-    for word in data.split():
-        print(word, end = "#")
+data = file.read()
+for word in data.split():
+    print(word, end = "#")
 
 file.close()
